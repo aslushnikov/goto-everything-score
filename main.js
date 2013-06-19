b63e166590491383385ec6eae427df3a78e8f17d
Andrey Lushnikov <aslushnikov@chromium.org>
2013-06-19 11:03:55 +0400

    spice up with args for better use

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
 var fs = require('fs')
   , colors = require("colors")
   , Runner = require("./lib/runner.js")
+  , program = require("commander")
 
-var query = process.argv[2];
-if (!query || !query.length) {
-    console.log("Error: pass query as an arg");
+program
+    .usage("-q QUERY [options] [entry1, entry2, ...]")
+    .option("-s, --score <scoring function path>", "run specific scoring function only")
+    .option("-q, --query <query>", "pass query to run")
+    .parse(process.argv);
+
+if (!program.query) {
+    program.outputHelp();
     process.exit(1);
 }
 
 var lines;
-if (process.argv.length > 3) {
-    lines = process.argv.slice(3);
+if (program.args.length > 0) {
+    lines = program.args.length;
 } else {
     lines = fs.readFileSync("samples/blink.txt", "utf-8").split('\n');
 }
 
 console.log("Initial lines size: " + lines.length);
-lines = Runner.filter(query, lines);
+lines = Runner.filter(program.query, lines);
 console.log("Filtered lines size: " + lines.length);
-
-var pf = new Runner("pfeldman", require("./lib/pf.js"));
-var ls = new Runner("lushnikov", require("./lib/ls.js"));
 
 function executeRunner(runner, query, lines) {
     console.time(runner.name());
@@ -29,6 +32,13 @@ function executeRunner(runner, query, lines) {
     console.log(runner.render(query, best, true));
 }
 
-executeRunner(pf, query, lines);
-executeRunner(ls, query, lines);
+if (program.score) {
+    var r = new Runner(program.score, require(program.score));
+    executeRunner(r, program.query, lines);
+} else {
+    var pf = new Runner("pfeldman", require("./lib/pf.js"));
+    var ls = new Runner("lushnikov", require("./lib/ls.js"));
+    executeRunner(pf, program.query, lines);
+    executeRunner(ls, program.query, lines);
+}
 
--- test/testset.js
+++ test/testset.js
@@ -1,10 +1,12 @@
 module.exports = [
-    ["dte", "./Source/devtools/front_end/DefaultTextEditor.js"]
-  , ["frodte", "./Source/devtools/front_end/DefaultTextEditor.js"]
+    ["textejs", "./Source/devtools/front_end/TextEditor.js"]
+  , ["defted", "./Source/devtools/front_end/DefaultTextEditor.js"]
   , ["cmte", "./Source/devtools/front_end/CodeMirrorTextEditor.js"]
   , ["frocmte", "./Source/devtools/front_end/CodeMirrorTextEditor.js"]
+  , ["cmtejs", "./Source/devtools/front_end/CodeMirrorTextEditor.js"]
   , ["setscr", "./Source/devtools/front_end/SettingsScreen.js"]
   , ["cssnfv", "./Source/devtools/front_end/CSSNamedFlowView.js"]
   , ["jssf", "./Source/devtools/front_end/JavaScriptSourceFrame.js"]
-  , ["sofra", "./Source/devtools/front_end/SourceFrame.js"]
+  , ["sofrajs", "./Source/devtools/front_end/SourceFrame.js"]
+  , ["inspeins", "./Source/core/inspector/InspectorInstrumentation.cpp"]
 ];
